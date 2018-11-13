3e00a1d1069328a03d049867a128cbb7a0d8e3fd
Aadhithya Sankar <a.sankar@tum.de>
2018-11-13 01:11:45 +0100

    Support for pip

--- build/lib/mvg_console/get_info.py
+++ build/lib/mvg_console/get_info.py
@@ -23,7 +23,7 @@ MVG_FG = "#ffffff"
 #######
 
 __package_name__ = "mvg_console"
-__version__ = "2018.11.12.1"
+__version__ = "2018.11.12.1.0.1"
 __description__ = "A Command Line Tool to get the MVG departures for a station."
 
 class Departure:
